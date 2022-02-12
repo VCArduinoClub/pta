8be6242cf0148208229fe8b3a65d41978041f8d9
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2022-02-11 22:21:23 -0800

    added spec

--- lib/pta_web/controllers/test_controller.ex
+++ lib/pta_web/controllers/test_controller.ex
@@ -6,10 +6,12 @@ defmodule PtaWeb.TestController do
     render(conn, "test.html")
   end
 
+  @spec index(Plug.Conn.t(), any) :: Plug.Conn.t()
   def index(conn, _param) do
     render(conn, "index.html")
   end
 
+  @spec show(Plug.Conn.t(), map) :: Plug.Conn.t()
   def show(conn, %{"id" => id}) do
     render(conn, "show.html", id: id<>"html")
   end
